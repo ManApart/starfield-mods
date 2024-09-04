20aa9c9e4410b1b4ab9f26d7c6c5f66f376d969b
ManApart <r@austinkucera.com>
2024-09-04 07:42:01 -0400

    working exporter

--- tools/src/main/kotlin/OstExporter.kt
+++ tools/src/main/kotlin/OstExporter.kt
@@ -14,22 +14,27 @@ fun main() {
 
     File(input).listFiles().forEach { folder ->
         println("\nProcessing ${folder.name}")
-        folder.listFiles().filter { !it.isDirectory }.forEach {
-            val cleanName = if (it.nameWithoutExtension.contains("_")) it.nameWithoutExtension.substring(0, it.nameWithoutExtension.lastIndexOf("_")) else it.nameWithoutExtension
+        processDirectory(folder, output, tool)
+    }
+}
 
-            val voiceFile = File("${output}/${folder.nameWithoutExtension}/${cleanName}.mp3")
-            if (!voiceFile.exists()) {
-                println(cleanName)
-                try {
+private fun processDirectory(folder: File, output: String, tool: File) {
+    folder.listFiles().filter { it.isDirectory }.forEach { processDirectory(it, output +"/${it.name}", tool) }
+    folder.listFiles().filter { !it.isDirectory }.forEach {
+        val cleanName = if (it.nameWithoutExtension.contains("_")) it.nameWithoutExtension.substring(0, it.nameWithoutExtension.lastIndexOf("_")) else it.nameWithoutExtension
 
-                    it.wemToWavVgm(tool)
-                    it.parentFile.runCommand(listOf("ffmpeg", "-i", "${it.nameWithoutExtension}.wav", "temp.mp3"))
-                    voiceFile.parentFile.mkdirs()
-                    Files.move(Path("${it.parentFile.absolutePath}/temp.mp3"), voiceFile.toPath())
-                    File(it.parentFile.absolutePath + "/${it.nameWithoutExtension}.wav").delete()
-                } catch (e: Exception) {
-                    println("Failed to process ${it.name}")
-                }
+        val voiceFile = File("${output}/${folder.nameWithoutExtension}/${cleanName}.mp3")
+        if (!voiceFile.exists()) {
+            println(cleanName)
+            try {
+
+                it.wemToWavVgm(tool)
+                it.parentFile.runCommand(listOf("ffmpeg", "-i", "${it.nameWithoutExtension}.wav", "temp.mp3"))
+                voiceFile.parentFile.mkdirs()
+                Files.move(Path("${it.parentFile.absolutePath}/temp.mp3"), voiceFile.toPath())
+                File(it.parentFile.absolutePath + "/${it.nameWithoutExtension}.wav").delete()
+            } catch (e: Exception) {
+                println("Failed to process ${it.name}")
             }
         }
     }
