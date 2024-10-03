abaa72bf5747ebbdd30492795db10320580a5098
ManApart <r@austinkucera.com>
2024-10-03 07:28:37 -0400

    extract DLC

--- tools/src/main/kotlin/SoundBankExtractor.kt
+++ tools/src/main/kotlin/SoundBankExtractor.kt
@@ -9,12 +9,23 @@ import java.nio.file.Files
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 private class Wrapper(
-    @JsonProperty("SoundBanksInfo") val soundBanksInfo: SoundBankInfo
+    @JsonProperty("SoundBanksInfo") val soundBanksInfo: SoundBankInfo,
 )
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 private class SoundBankInfo(
-    @JsonProperty("StreamedFiles") val streamedFiles: List<StreamedFile> = listOf()
+    @JsonProperty("StreamedFiles") val streamedFiles: List<StreamedFile> = listOf(),
+    @JsonProperty("SoundBanks") val soundBanks: List<SoundBank>
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+private class SoundBank(
+    @JsonProperty("IncludedEvents") val includedEvent: List<IncludedEvent> = listOf()
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+private class IncludedEvent(
+    @JsonProperty("ReferencedStreamedFiles") val streamedFiles: List<StreamedFile> = listOf()
 )
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -45,10 +56,14 @@ private fun moveFiles(source: File, destination: File) {
         sounds.take(LIMIT).chunked(CHUNK_SIZE).forEach { chunk ->
             chunk.map { sound ->
                 async(Dispatchers.IO) {
-                    val sourcePath = File("${source.absolutePath}/soundbanks/${sound.id}.wem").toPath()
-                    val target = File(destination.absolutePath + "/" + sound.path.replace(".wem", "").replace("\\", "/") + "-${sound.id}.wem").also { it.parentFile.mkdirs() }
-                    if (!target.exists()) {
-                        Files.copy(sourcePath, target.toPath())
+                    val source = File("${source.absolutePath}/soundbanks/${sound.id}.wem")
+                    val target = File(destination.absolutePath + "/" + sound.path.replace(".wem", "").replace("\\", "/") + "-${sound.id}.wem")
+                        .also { it.parentFile.mkdirs() }
+                    if (!source.exists()) {
+                        println("Skipping ${sound.shortName}; source missing")
+                    } else if (!target.exists()) {
+//                        println("$sourcePath\n\t$target")
+                        Files.copy(source.toPath(), target.toPath())
                     }
                 }
             }.awaitAll()
@@ -61,5 +76,6 @@ private fun moveFiles(source: File, destination: File) {
 private fun parseFileInfo(source: File): List<StreamedFile> {
     val infoFile = File("${source.absolutePath}/soundbanks/soundbanksinfo.json")
     val wrapper: Wrapper = mapper.readValue(infoFile)
-    return wrapper.soundBanksInfo.streamedFiles
+    val allFiles = wrapper.soundBanksInfo.streamedFiles + wrapper.soundBanksInfo.soundBanks.flatMap { bank -> bank.includedEvent.flatMap { it.streamedFiles } }
+    return allFiles.groupBy { it.path }.values.map { it.first() }
 }
