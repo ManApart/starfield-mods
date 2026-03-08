3e1fd25cf844e5689da5342464282281edbaa57b
ManApart <r@austinkucera.com>
2026-03-08 18:51:41 -0400

    use scripts folder

--- auto-sort/backup.sh
+++ auto-sort/backup.sh
@@ -1,14 +1,14 @@
 DATA=/mnt/extradrive1/SteamLibrary/steamapps/common/Starfield/Data
 cp $DATA/AkAutoSort.esp ./auto-sort/AkAutoSort.esp
-cp $DATA/scripts/source/AKSortTool.psc ./auto-sort/AKSortTool.psc
-cp $DATA/scripts/source/AKAutoSortQuest.psc ./auto-sort/AKAutoSortQuest.psc
-cp $DATA/scripts/source/QF_AKAutoSort_01000806.psc ./auto-sort/QF_AKAutoSort_01000806.psc
-cp $DATA/scripts/source/AKASViewKeywords.psc ./auto-sort/AKASViewKeywords.psc
-cp $DATA/scripts/source/fragments/terminals/TERM_AKASMainMenu_0100083F.psc ./auto-sort/TERM_AKASMainMenu_0100083F.psc
-cp $DATA/scripts/source/fragments/terminals/TERM_AKASTagObjectType_01000E9E.psc ./auto-sort/TERM_AKASTagObjectType_01000E9E.psc
-cp $DATA/scripts/source/fragments/terminals/TERM__01000E9B.psc ./auto-sort/TERM__01000E9B.psc
-cp $DATA/scripts/source/fragments/terminals/TERM__01000EA3.psc ./auto-sort/TERM__01000EA3.psc
-cp $DATA/scripts/source/fragments/terminals/TERM__01000EA4.psc ./auto-sort/TERM__01000EA4.psc
-cp $DATA/scripts/source/fragments/terminals/TERM__01000EA5.psc ./auto-sort/TERM__01000EA5.psc
-cp $DATA/scripts/source/fragments/terminals/TERM__01000EAC.psc ./auto-sort/TERM__01000EAC.psc
-cp $DATA/scripts/source/fragments/terminals/TERM__01000ECD.psc ./auto-sort/TERM__01000ECD.psc
+cp $DATA/scripts/source/AKSortTool.psc ./auto-sort/scripts/AKSortTool.psc
+cp $DATA/scripts/source/AKAutoSortQuest.psc ./auto-sort/scripts/AKAutoSortQuest.psc
+cp $DATA/scripts/source/QF_AKAutoSort_01000806.psc ./auto-sort/scripts/QF_AKAutoSort_01000806.psc
+cp $DATA/scripts/source/AKASViewKeywords.psc ./auto-sort/scripts/AKASViewKeywords.psc
+cp $DATA/scripts/source/fragments/terminals/TERM_AKASMainMenu_0100083F.psc ./auto-sort/scripts/TERM_AKASMainMenu_0100083F.psc
+cp $DATA/scripts/source/fragments/terminals/TERM_AKASTagObjectType_01000E9E.psc ./auto-sort/scripts/TERM_AKASTagObjectType_01000E9E.psc
+cp $DATA/scripts/source/fragments/terminals/TERM__01000E9B.psc ./auto-sort/scripts/TERM__01000E9B.psc
+cp $DATA/scripts/source/fragments/terminals/TERM__01000EA3.psc ./auto-sort/scripts/TERM__01000EA3.psc
+cp $DATA/scripts/source/fragments/terminals/TERM__01000EA4.psc ./auto-sort/scripts/TERM__01000EA4.psc
+cp $DATA/scripts/source/fragments/terminals/TERM__01000EA5.psc ./auto-sort/scripts/TERM__01000EA5.psc
+cp $DATA/scripts/source/fragments/terminals/TERM__01000EAC.psc ./auto-sort/scripts/TERM__01000EAC.psc
+cp $DATA/scripts/source/fragments/terminals/TERM__01000ECD.psc ./auto-sort/scripts/TERM__01000ECD.psc
